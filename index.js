0c12073d4c6a2aa356dce0d7250d5061b18e7855
Vienna Borowska <vienna.borowska@moneysupermarket.com>
2023-10-25 09:51:56 +0100

    added driver code to run program up to 47 spaces. Left in comments of old driver code where program has been tested against manual calculations. Data being written to csv file.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 //Parking Space Arrangement Calculator
-
 //(M) Motorcycle = 1 space
 //(C) Car = 2 spaces
 //(V) Van = 3 spaces
@@ -13,9 +12,21 @@
 //IF spaces available = 1, return 1 (base case 1)
 //IF spaces available = 2, return 2 (base case 2)
 //IF spaces available = 3, return 4 (base case 3)
-// ELSE call function to calculate spaces down until base cases are met and stack return value
+//ELSE calculate available spaces by splitting spaces down into manageable chunks i.e when base cases are met
+//RETURN all possible parking arrangements as number
+
+const fs = require("fs");
+const { performance } = require("perf_hooks");
+
+if (fs.existsSync("recursiveParkingRuntimes.csv"))
+  fs.unlinkSync("recursiveParkingRuntimes.csv");
+// create and write a header to the output csv file
+fs.appendFile("recursiveParkingRuntimes.csv", "N, Average Runtime\n", (err) => {
+  if (err) throw err;
+});
 
 function parkingSpaceArrangement(numOfSpaces) {
+  //Outline 3 base cases, one for each vehicle
   if (numOfSpaces == 1) {
     return 1;
   } else if (numOfSpaces == 2) {
@@ -23,6 +34,7 @@ function parkingSpaceArrangement(numOfSpaces) {
   } else if (numOfSpaces == 3) {
     return 4;
   } else {
+    //Increment down number of spaces using recursive calls to function to calculate data in manageable sizes according to outlined base cases
     return (
       parkingSpaceArrangement(numOfSpaces - 1) +
       parkingSpaceArrangement(numOfSpaces - 2) +
@@ -31,7 +43,25 @@ function parkingSpaceArrangement(numOfSpaces) {
   }
 }
 
-console.log(parkingSpaceArrangement(4));
-console.log(parkingSpaceArrangement(5));
-console.log(parkingSpaceArrangement(6));
-console.log(parkingSpaceArrangement(7));
+//Driver & Testing Code
+for (let parkingSize = 1; parkingSize <= 47; parkingSize++) {
+  //Calculate runtime
+  let timestart = performance.now();
+  parkingSpaceArrangement(parkingSize);
+  let runtime = performance.now() - timestart;
+  console.log(runtime);
+
+  let data = `${parkingSize}, ${runtime}\n`;
+  fs.appendFile("recursiveParkingRuntimes.csv", data, (err) => {
+    if (err) throw err;
+  });
+}
+
+// Testing function against manual calculations
+// console.log(parkingSpaceArrangement(1));
+// console.log(parkingSpaceArrangement(2));
+// console.log(parkingSpaceArrangement(3));
+// console.log(parkingSpaceArrangement(4));
+// console.log(parkingSpaceArrangement(5));
+// console.log(parkingSpaceArrangement(6));
+// console.log(parkingSpaceArrangement(7));
